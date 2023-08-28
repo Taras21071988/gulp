5098f2e38f1676caa385ee4710d6315f4099dba7
Taras21071988 <tsasojlov@icloud.com>
2023-08-28 14:43:03 +0300

    connection autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 
 const scss = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
 
+//Работа с файлами стилей + autoprefixer
 function styles() {
   return src("app/scss/style.scss")
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 version"], // Указываем поддержка скольки предыдующих версий нужна
+      })
+    )
     .pipe(concat("style.min.css"))
     .pipe(scss({ outputStyle: "compressed" }))
     .pipe(dest("app/css"))
     .pipe(browserSync.stream());
 }
 
+//Работа с файлами скриптов
 function scripts() {
   return src("app/js/main.js")
     .pipe(concat("main.min.js"))
@@ -20,6 +28,8 @@ function scripts() {
     .pipe(dest("app/js"))
     .pipe(browserSync.stream());
 }
+
+//Обновление браузера
 function browsersync() {
   browserSync.init({
     server: {
@@ -27,13 +37,17 @@ function browsersync() {
     },
   });
 }
-
+//Отслеживание изменений в описанных тут файлах
 function watching() {
   watch(["app/scss/style.scss"], styles);
   watch(["app/js/main.js"], scripts);
   watch(["app/*.html"]).on("change", browserSync.reload);
 }
+
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
+
+//Параллельный запуск всех требуемых tasks
+exports.default = parallel(styles, scripts, browsersync, watching);
