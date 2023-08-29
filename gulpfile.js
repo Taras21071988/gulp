d414e49a9f802bb6b2718d4f2ca385ba55de90bb
Taras21071988 <tsasojlov@icloud.com>
2023-08-29 11:32:54 +0300

    creating a task for build and clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-const { src, dest, watch, parallel } = require("gulp");
+const { src, dest, watch, parallel, series } = require("gulp");
 
 const scss = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
+const clean = require("gulp-clean");
 
 //Работа с файлами стилей + autoprefixer
 function styles() {
@@ -22,7 +23,11 @@ function styles() {
 
 //Работа с файлами скриптов
 function scripts() {
-  return src("app/js/main.js")
+  return src([
+    // "node_modules/swiper/swiper-bundle.js", //Подключение для тестов swiper
+    "app/js/**/*.js", //Работа со всеми файлами которые имеют расширение .js внутри папки js - и вложенных папок в ней
+    "!app/js/main.min.js", //Указываем что неиспользовать данный файл
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
@@ -40,8 +45,20 @@ function browsersync() {
 //Отслеживание изменений в описанных тут файлах
 function watching() {
   watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
+  watch(["app/js/**/*.js"], scripts);
   watch(["app/*.html"]).on("change", browserSync.reload);
+}
+
+//Task для группировки конечных файлов перед выдачей
+function building() {
+  return src(["app/css/style.min.css", "app/js/main.min.js", "app/**/*.html"], {
+    base: "app",
+  }).pipe(dest("dist"));
+}
+
+//Task для удаление папки dist при повторнои использование build
+function cleanDist() {
+  return src("dist").pipe(clean());
 }
 
 exports.styles = styles;
@@ -49,5 +66,8 @@ exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 
-//Параллельный запуск всех требуемых tasks
+//Выполнение build and clean - используется series(последовательное выполнение task)
+exports.build = series(cleanDist, building);
+
+//Параллельный запуск всех требуемых tasks - используется parallel(task выполняется паралельно друг другу)
 exports.default = parallel(styles, scripts, browsersync, watching);
